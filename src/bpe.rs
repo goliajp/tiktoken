ed0e7b989ddeb1a094d4eaad281e9890c9e1db1a
Panda <lihao@golia.jp>
2023-08-18 10:46:43 +0800

    Initial commit

---
+++ src/bpe.rs
@@ -0,0 +1,224 @@
+use anyhow::Error;
+use pcre2::bytes::{Regex, RegexBuilder};
+use std::collections::HashMap;
+
+pub struct CoreBpe {
+    encoder: HashMap<String, isize>,
+    decoder: HashMap<isize, String>,
+    special_tokens_encoder: HashMap<String, isize>,
+    special_tokens_decoder: HashMap<isize, String>,
+    tl_regex: Regex,
+    tl_special_regex: Regex,
+    sorted_token_bytes: Vec<Vec<u8>>,
+}
+
+impl CoreBpe {
+    pub fn new(
+        encoder: HashMap<String, isize>,
+        special_tokens_encoder: HashMap<String, isize>,
+        pattern: String,
+    ) -> Result<Self, Error> {
+        // build regex
+        let tl_regex = RegexBuilder::new()
+            .jit_if_available(true)
+            .ucp(true)
+            .utf(true)
+            .build(pattern.as_str())?;
+        let special_regex_strings: Vec<String> = special_tokens_encoder
+            .iter()
+            .map(|token| pcre2::escape(token.0).to_string())
+            .collect();
+        let special_regex_pattern = special_regex_strings.join("|");
+        let tl_special_regex = Regex::new(&special_regex_pattern)?;
+
+        // create decoder
+        let mut decoder = HashMap::new();
+        for (key, value) in encoder.clone().into_iter() {
+            decoder.insert(value, key);
+        }
+
+        // create special tokens decoder
+        let mut special_tokens_decoder = HashMap::new();
+        for (key, value) in special_tokens_encoder.clone().into_iter() {
+            special_tokens_decoder.insert(value, key);
+        }
+
+        // sort token bytes
+        let mut sorted_token_bytes: Vec<Vec<u8>> =
+            encoder.keys().map(|s| s.as_bytes().to_vec()).collect();
+        sorted_token_bytes.sort_by(|a, b| a.cmp(b));
+
+        Ok(Self {
+            encoder,
+            special_tokens_encoder,
+            decoder,
+            special_tokens_decoder,
+            tl_regex,
+            tl_special_regex,
+            sorted_token_bytes,
+        })
+    }
+
+    pub fn encode_native(&self, text: &str) -> (Vec<isize>, usize) {
+        let mut result = vec![];
+        let mut last_piece_token_len = 0;
+        let mut start = 0;
+        let text_chars: Vec<char> = text.chars().collect();
+
+        // encoding
+        loop {
+            let mut next_special = None;
+            let mut start_find = start;
+            loop {
+                // find next special token
+                let temp = cut_chars(&text_chars, start_find, text_chars.len());
+                next_special = find_regex_to_string_index(&temp, &self.tl_special_regex);
+                if next_special.is_none() {
+                    break;
+                }
+                start_find += next_special.unwrap().1;
+            }
+
+            // confirm text slice end
+            let mut end = text_chars.len();
+            if !next_special.is_none() {
+                end = start + next_special.unwrap().0;
+            }
+
+            // handle text
+            let temp = cut_chars(&text_chars, start, end);
+            let store = find_regex_to_all_string_match_index(&temp, &self.tl_regex);
+            for matched in store {
+                let piece = cut_chars(&text_chars, start + matched.0, start + matched.1);
+                if let Some(&token) = self.encoder.get(&piece) {
+                    last_piece_token_len = 1;
+                    result.push(token);
+                    continue;
+                }
+
+                // encode byte pair
+                let tokens = byte_pair_encode(piece.as_bytes(), &self.encoder);
+                last_piece_token_len = tokens.len();
+                result.extend(tokens);
+            }
+
+            // handle next special token
+            if let Some(matched) = next_special {
+                let temp = cut_chars(&text_chars, start + matched.0, start + matched.1);
+                if let Some(&token) = self.special_tokens_encoder.get(temp.as_str()) {
+                    result.push(token);
+                    start += matched.1;
+                    last_piece_token_len = 0;
+                }
+            } else {
+                break;
+            }
+        }
+        (result, last_piece_token_len)
+    }
+}
+
+fn cut_chars(chars: &Vec<char>, start: usize, end: usize) -> String {
+    let start = start.min(chars.len());
+    let end = end.min(chars.len());
+    chars[start..end].iter().collect()
+}
+
+fn find_regex_to_string_index(text: &str, regex: &Regex) -> Option<(usize, usize)> {
+    if let Some(matched) = regex.find(text.as_bytes()).unwrap() {
+        let matched_str = std::str::from_utf8(matched.as_bytes()).unwrap();
+        let start = text[..matched.start()].chars().count();
+        let end = start + matched_str.chars().count();
+        Some((start, end))
+    } else {
+        None
+    }
+}
+
+fn find_regex_to_all_string_match_index(text: &str, regex: &Regex) -> Vec<(usize, usize)> {
+    regex
+        .find_iter(text.as_bytes())
+        .map(|matched| {
+            let matched = matched.unwrap();
+            let matched_str = std::str::from_utf8(matched.as_bytes()).unwrap();
+            let start = text[..matched.start()].chars().count();
+            let end = start + matched_str.chars().count();
+            (start, end)
+        })
+        .collect()
+}
+
+fn byte_pair_encode(piece: &[u8], ranks: &HashMap<String, isize>) -> Vec<isize> {
+    if piece.len() == 1 {
+        unsafe {
+            let key = String::from_utf8_unchecked(Vec::from(piece));
+            let value = *ranks.get(&key).unwrap();
+            return vec![value];
+        }
+    }
+    byte_pair_merge(piece, ranks, |start, end| -> isize {
+        let slice = piece[start..end].to_vec();
+        unsafe {
+            let key = String::from_utf8_unchecked(slice);
+            ranks.get(&key).cloned().unwrap_or(0)
+        }
+    })
+}
+
+fn byte_pair_merge<T, F>(piece: &[u8], ranks: &HashMap<String, isize>, f: F) -> Vec<T>
+where
+    F: Fn(usize, usize) -> T,
+{
+    let mut parts: Vec<[usize; 2]> = (0..piece.len() + 1).map(|i| [i, usize::MAX]).collect();
+    let get_rank = |start_idx: usize, skip: usize, parts: &Vec<[usize; 2]>| -> isize {
+        if start_idx + skip + 2 < parts.len() {
+            let b = &piece[parts[start_idx][0]..parts[start_idx + skip + 2][0]];
+            unsafe {
+                let key = String::from_utf8_unchecked(b.to_vec());
+                if let Some(&rank) = ranks.get(&key) {
+                    return rank;
+                }
+            }
+        }
+        -1
+    };
+    for i in 0..parts.len() - 2 {
+        let rank = get_rank(i, 0, &parts);
+        if rank >= 0 {
+            parts[i][1] = rank as usize;
+        }
+    }
+    while parts.len() > 1 {
+        let mut min_rank = usize::MAX;
+        let mut min_idx: isize = -1;
+        for i in 0..parts.len() - 1 {
+            if parts[i][1] < min_rank {
+                min_rank = parts[i][1];
+                min_idx = i as isize;
+            }
+        }
+        if min_rank < usize::MAX {
+            let i = min_idx;
+            let rank = get_rank(i as usize, 1, &parts);
+            if rank >= 0 {
+                parts[i as usize][1] = rank as usize;
+            } else {
+                parts[i as usize][1] = usize::MAX;
+            }
+            if i > 0 {
+                let rk = get_rank((i - 1) as usize, 1, &parts);
+                if rk > 0 {
+                    parts[(i - 1) as usize][1] = rk as usize;
+                } else {
+                    parts[(i - 1) as usize][1] = usize::MAX;
+                }
+            }
+            parts.remove(i as usize + 1);
+        } else {
+            break;
+        }
+    }
+    (0..parts.len() - 1)
+        .map(|i| f(parts[i][0], parts[i + 1][0]))
+        .collect()
+}
---
+++ src/encoding.rs
@@ -0,0 +1,211 @@
+use anyhow::{anyhow, Error};
+use base64::Engine;
+use lazy_static::lazy_static;
+use maplit::hashmap;
+use std::collections::HashMap;
+
+// enums
+
+#[derive(Eq, PartialEq, Hash)]
+pub enum SpecialToken {
+    EndOfText,
+    FimPrefix,
+    FimMiddle,
+    FimSuffix,
+    EndOfPrompt,
+}
+
+impl SpecialToken {
+    pub fn to_string(&self) -> String {
+        match self {
+            Self::EndOfText => String::from("<|endoftext|>"),
+            Self::FimPrefix => String::from("<|fim_prefix|>"),
+            Self::FimMiddle => String::from("<|fim_middle|>"),
+            Self::FimSuffix => String::from("<|fim_suffix|>"),
+            Self::EndOfPrompt => String::from("<|endofprompt|>"),
+        }
+    }
+}
+
+pub enum Dict {
+    Cl100kBase,
+    P50kBase,
+    P50kEdit,
+    R50kBase,
+}
+
+impl Dict {
+    pub fn to_string(&self) -> String {
+        match self {
+            Self::Cl100kBase => String::from("cl100k_base"),
+            Self::P50kBase => String::from("p50k_base"),
+            Self::P50kEdit => String::from("p50k_edit"),
+            Self::R50kBase => String::from("r50k_base"),
+        }
+    }
+}
+
+#[derive(Eq, PartialEq, Hash)]
+pub enum ChatModelPrefix {
+    Gpt3dot5,
+    Gpt4,
+}
+
+impl ChatModelPrefix {
+    pub fn to_string(&self) -> String {
+        match self {
+            Self::Gpt4 => String::from("gpt-4"),
+            Self::Gpt3dot5 => String::from("gpt-3.5"),
+        }
+    }
+
+    pub fn get_by_name(chat_model_name: &str) -> Result<Self, Error> {
+        match chat_model_name.to_lowercase() {
+            _ if chat_model_name.starts_with(Self::Gpt3dot5.to_string().as_str()) => {
+                Ok(Self::Gpt3dot5)
+            }
+            _ if chat_model_name.starts_with(Self::Gpt4.to_string().as_str()) => Ok(Self::Gpt4),
+            _ => Err(anyhow!("no prefix for model {}", chat_model_name)),
+        }
+    }
+}
+
+// mapping
+
+lazy_static! {
+    pub static ref CHAT_MODEL_PREFIX_TO_DICT: HashMap<ChatModelPrefix, Dict> = {
+        let mut map = HashMap::new();
+        map.insert(ChatModelPrefix::Gpt3dot5, Dict::Cl100kBase);
+        map.insert(ChatModelPrefix::Gpt4, Dict::Cl100kBase);
+        map
+    };
+}
+
+// encoding
+
+pub struct Encoding {
+    pub dict: Dict,
+    pub pattern: String,
+    pub merging_ranks: HashMap<String, isize>,
+    pub special_tokens: HashMap<String, isize>,
+    pub explicit_vocab_size: isize,
+}
+
+impl Encoding {
+    pub fn get_by_dict(dict: &Dict) -> Result<Self, Error> {
+        match dict {
+            Dict::Cl100kBase => cl100k_base(),
+            Dict::P50kBase => p50k_base(),
+            Dict::P50kEdit => p50k_edit(),
+            Dict::R50kBase => r50k_base(),
+        }
+    }
+
+    pub fn get_by_chat_model(chat_model_name: &str) -> Result<Self, Error> {
+        let model_prefix = ChatModelPrefix::get_by_name(chat_model_name)?;
+        match CHAT_MODEL_PREFIX_TO_DICT.get(&model_prefix) {
+            Some(dict) => Encoding::get_by_dict(dict),
+            None => Err(anyhow!(
+                "no encoding for model prefix {}",
+                model_prefix.to_string()
+            )),
+        }
+    }
+}
+
+fn cl100k_base() -> Result<Encoding, Error> {
+    let dict_data = include_bytes!("encodings/cl100k_base.tiktoken");
+    let merging_ranks = parse_dict_data(dict_data)?;
+    let special_tokens = hashmap! {
+        SpecialToken::EndOfText.to_string() => 100257,
+        SpecialToken::FimPrefix.to_string() => 100258,
+        SpecialToken::FimMiddle.to_string() => 100259,
+        SpecialToken::FimSuffix.to_string() => 100260,
+        SpecialToken::EndOfPrompt.to_string() => 100276,
+    };
+
+    Ok(Encoding {
+        dict: Dict::Cl100kBase,
+        pattern: r"(?i:'s|'t|'re|'ve|'m|'ll|'d)|[^\r\n\p{L}\p{N}]?\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]+[\r\n]*|\s*[\r\n]+|\s+(?!\S)|\s+".to_string(),
+        merging_ranks,
+        special_tokens,
+        explicit_vocab_size: 0,
+    })
+}
+
+fn p50k_edit() -> Result<Encoding, Error> {
+    let dict_data = include_bytes!("encodings/p50k_base.tiktoken");
+    let merging_ranks = parse_dict_data(dict_data)?;
+    let special_tokens = hashmap! {
+        SpecialToken::EndOfText.to_string() => 50256,
+        SpecialToken::FimPrefix.to_string() => 50281,
+        SpecialToken::FimMiddle.to_string() => 50282,
+        SpecialToken::FimSuffix.to_string() => 50283,
+    };
+
+    Ok(Encoding {
+        dict: Dict::P50kEdit,
+        pattern: r"(?i:'s|'t|'re|'ve|'m|'ll|'d)|[^\r\n\p{L}\p{N}]?\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]+[\r\n]*|\s*[\r\n]+|\s+(?!\S)|\s+"
+            .to_string(),
+        merging_ranks,
+        special_tokens,
+        explicit_vocab_size: 0,
+    })
+}
+
+fn p50k_base() -> Result<Encoding, Error> {
+    let dict_data = include_bytes!("encodings/p50k_base.tiktoken");
+    let merging_ranks = parse_dict_data(dict_data)?;
+    let special_tokens = hashmap! {
+        SpecialToken::EndOfText.to_string() => 50256,
+    };
+
+    Ok(Encoding {
+        dict: Dict::P50kBase,
+        pattern: r"'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"
+            .to_string(),
+        merging_ranks,
+        special_tokens,
+        explicit_vocab_size: 50281,
+    })
+}
+
+fn r50k_base() -> Result<Encoding, Error> {
+    let dict_data = include_bytes!("encodings/r50k_base.tiktoken");
+    let merging_ranks = parse_dict_data(dict_data)?;
+    let special_tokens = hashmap! {
+        SpecialToken::EndOfText.to_string() => 50256,
+    };
+
+    Ok(Encoding {
+        dict: Dict::R50kBase,
+        pattern: r"'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"
+            .to_string(),
+        merging_ranks,
+        special_tokens,
+        explicit_vocab_size: 50257,
+    })
+}
+
+fn parse_dict_data(contents: &[u8]) -> Result<HashMap<String, isize>, Error> {
+    let mut bpe_ranks = HashMap::new();
+    let engine = base64::engine::general_purpose::STANDARD;
+    unsafe {
+        let content_str = String::from_utf8_unchecked(Vec::from(contents));
+        for line in content_str.lines() {
+            if line.trim() == "" {
+                continue;
+            }
+            let parts: Vec<&str> = line.split(" ").collect();
+            if parts.len() != 2 {
+                return Err(anyhow!("unexpected line format: {}", line));
+            }
+            let token_bytes = engine.decode(parts[0])?;
+
+            let token = String::from_utf8_unchecked(token_bytes);
+            let rank: isize = parts[1].parse()?;
+            bpe_ranks.insert(token, rank);
+        }
+    }
+    Ok(bpe_ranks)
+}
